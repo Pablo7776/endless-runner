b8513395b434d8cdfc46de690ed59f157dbb8a37
Jipilucas <zvitsova@gmail.com>
2025-11-19 12:08:52 -0300

    aparecible y objeto

---
+++ scripts/objeto.gd
@@ -0,0 +1,12 @@
+@abstract class_name Objeto
+extends IAparecible
+
+var __momento_de_aparicion:float
+
+@abstract func __aparecer(lista_de_momentos) -> float
+
+'''Objeto(Ubicable)
+_area_de_colision: Area2D 
+_posicion_fija:parordenado
+--------
+-ubicar (lista_de_ubicaciones): parordenado'''
