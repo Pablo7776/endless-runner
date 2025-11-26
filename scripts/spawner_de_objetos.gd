fc757e3055969a2681763462c2561a0fab7b52e2
Jipilucas <zvitsova@gmail.com>
2025-11-26 15:36:13 -0300

    Spawn

--- scripts/spawner_de_objetos.gd
+++ scripts/spawner_de_objetos.gd
@@ -1,11 +1,12 @@
 extends Node2D
-class_name  SpawnerDeObjetos
+class_name  SpawnerVariable
 
-var objetos = [
+var aparecibles = [
 	preload("res://escenas/Objetos/placa_antibalas.tscn"),
 	preload("res://escenas/Objetos/tubo_tumbero.tscn"),
 	preload("res://escenas/Objetos/vida_extra.tscn"),
-	preload("res://escenas/enemigo_spawneable1.tscn")
+	preload("res://escenas/enemigo_spawneable1.tscn"),
+	preload("res://escenas/enemigo_2.tscn")
 	]
 
 @export var tiempo_spawn := 5 
@@ -18,10 +19,10 @@ func _ready():
 func esperar_para_spawn():
 	while true:
 		await get_tree().create_timer(tiempo_spawn).timeout
-		spawnear_objeto()
+		spawnear()
 
-func spawnear_objeto():
-	var escena = objetos.pick_random()
+func spawnear():
+	var escena = aparecibles.pick_random()
 	var inst = escena.instantiate()
 	inst.global_position = posicion_spawn.global_position
 	get_tree().current_scene.add_child(inst)
